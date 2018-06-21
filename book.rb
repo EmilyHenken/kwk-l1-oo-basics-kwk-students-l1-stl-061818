b3146c0e4c8bb43e5e4e84711e7f4c7f36b8c7e1
EmilyHenken <20henkee@corjesu.org>
2018-06-21 20:13:44 +0000

    automatically backed up by learn

--- book.rb
+++ book.rb
@@ -12,3 +12,4 @@ end
 
 book1 = Book.new("Susane Collins", "Hunger Games", "dystopian sci fi", 374)
 book2 = Book.new("Louis Lowrey", "The Giver", "dystopian", 240)
+book3 = Book.new("JK Rowling", "HP and Prisoner of Azkaban",)
